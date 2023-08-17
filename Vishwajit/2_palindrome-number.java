894dd5be00abc1988755a87d60a6e2db3374f1a3
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2023-08-17 23:15:50 +0530

    Create 2_palindrome-number.java

---
+++ Vishwajit/2_palindrome-number.java
@@ -0,0 +1,20 @@
+// palindrome-number - leetcode question.
+// Problem link - https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        
+        int temp =0;
+        int m = x;
+        int rem;
+        while(x>0){
+            rem = x%10;
+            temp = temp *10  + rem;
+            x = x/10;
+        }
+      
+        return temp==m ? true:false;
+     
+    }
+}
