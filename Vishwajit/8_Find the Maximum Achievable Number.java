8e0b4bfa218b5882f3d1101e6c1b89db8c3fe7c9
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2023-08-24 23:57:24 +0530

    Create 8_Find the Maximum Achievable Number.java

---
+++ Vishwajit/8_Find the Maximum Achievable Number.java
@@ -0,0 +1,7 @@
+// problem link - https://leetcode.com/problems/find-the-maximum-achievable-number/
+
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + (2 * t);
+    }
+}
