ca3cf3a6c8e6ad73a99934adc605e5e71c167bb7
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2023-10-05 13:03:22 +0530

    Create 32_Design HashMap.java

---
+++ Vishwajit/32_Design HashMap.java
@@ -0,0 +1,29 @@
+//  Design HashMap
+// https://leetcode.com/problems/design-hashmap
+class MyHashMap {
+    public int[] map;
+    public MyHashMap() {
+        map = new int[10000000];
+        Arrays.fill(map,-1);
+    }
+    
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    public int get(int key) {
+        return map[key];
+    }
+    
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
