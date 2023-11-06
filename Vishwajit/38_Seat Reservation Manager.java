2cbb38441df23cb960a2a2706a8bdb6b3d6e27ab
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2023-11-06 14:14:58 +0530

    Create 38_Seat Reservation Manager.java

---
+++ Vishwajit/38_Seat Reservation Manager.java
@@ -0,0 +1,25 @@
+// Seat Reservation Manager
+// https://leetcode.com/problems/seat-reservation-manager
+
+class SeatManager {
+    public PriorityQueue<Integer> queue = new PriorityQueue<>();
+    public SeatManager(int n) {
+        for(int i =1; i<=n ; i++){
+            queue.add(i);
+        }
+    }
+    public int reserve() {
+        return queue.poll();
+    }
+    
+    public void unreserve(int seatNumber) {
+        queue.add(seatNumber);
+    }
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */
